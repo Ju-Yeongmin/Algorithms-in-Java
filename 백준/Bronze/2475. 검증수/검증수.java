2e9fc8c0795d111260906da0353173fcc4f264d6
Ju-Yeongmin <110506500+Ju-Yeongmin@users.noreply.github.com>
2022-09-21 20:15:30 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int a3 = sc.nextInt();
+		int a4 = sc.nextInt();
+		int a5 = sc.nextInt();
+		
+		int b = ((a1*a1)+(a2*a2)+(a3*a3)+(a4*a4)+(a5*a5)) % 10;
+		System.out.println(b);
+	}
+}
