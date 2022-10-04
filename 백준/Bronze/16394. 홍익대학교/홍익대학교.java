d48363707b44fe32402de257bdafb4f4146dae1c
Ju-Yeongmin <110506500+Ju-Yeongmin@users.noreply.github.com>
2022-10-04 23:47:05 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		if (n >= 1946 && n <= 1000000) {
+			System.out.println(n-1946);
+			
+		}
+			
+		
+	}
+}
